563e0d67fa40e669b5bfc32a22162092b8c1a297
rafyarvelo <pi@raspberrypi.(none)>
2015-09-11 17:13:14 +0000

    Commiting emokit Build

---
+++ emokit/examples/contact/CMakeFiles/contact.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/home/pi/SENIOR_DESIGN/SMART_Software/emokit/examples/contact/contact.c" "/home/pi/SENIOR_DESIGN/SMART_Software/emokit/examples/contact/CMakeFiles/contact.dir/contact.c.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/pi/SENIOR_DESIGN/SMART_Software/emokit/src/CMakeFiles/emokit.dir/DependInfo.cmake"
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  "include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
