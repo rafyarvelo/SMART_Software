45b4a903a3bdf077d42118dcdb8e9b1f944d8a2f
Rafael Arvelo <rafaelarvelo1@gmail.com>
2015-09-22 23:00:26 -0400

    Full JA and TM Functionality

---
+++ RUN_SYSTEM.sh
@@ -0,0 +1,26 @@
+#This Executable should contain all the necessary commands for running the system
+
+#Executable Names
+export BCI_EXECUTABLE="BCI/BCI"
+export TM_EXTRACT_TOOL="BCI/TM_EXTRACT/TelemetryExtractTool"
+
+#Shared Libraries are included in the Build, Not usually a good idea but hey whatever man
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/lib:/usr/local/lib:$HOME/SENIOR_DESIGN/SMART_Software/BCI/lib"
+
+#Gotta change TOOLpermissions for those stupid serial ports
+deviceName=`dmesg | grep tty | grep USB | cut -d " " -f 4`
+deviceName=${deviceName/:/}
+
+echo "Unlocking Permissions for $deviceName..."
+sudo chmod 777 /dev/$deviceName
+
+#Compile Source Using Top Level MakeFile
+make
+
+#Run the Program
+sudo chmod +x $BCI_EXECUTABLE
+./$BCI_EXECUTABLE
+
+#Extract Telemetry when Program Finishes (Usually via Ctrl-C)
+sudo chmod +x $TM_EXTRACT_TOOL
+./$TM_EXTRACT_TOOL
