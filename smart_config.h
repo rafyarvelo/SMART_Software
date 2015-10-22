0f153fdd6a9d7eea0da7ee4b1c80deb80f5266ed
Rafy <rafaelarvelo1@gmail.com>
2015-10-21 21:37:38 -0400

    Added More Missing Files...

--- smart_config.h
+++ smart_config.h
@@ -25,11 +25,12 @@
 #endif
 
 //Common Typedefs
-typedef unsigned char  PCC_Command_Type;
-typedef unsigned char  uint8_t;
-typedef unsigned short uint16_t;
-typedef unsigned int   uint32_t;
-typedef unsigned int   sizeType;
+typedef unsigned char      PCC_Command_Type;
+typedef unsigned char      uint8_t;
+typedef unsigned short     uint16_t;
+typedef unsigned int       uint32_t;
+typedef unsigned long long uint64_t;
+typedef unsigned int       sizeType;
 
 #define TRUE          1
 #define FALSE         0
